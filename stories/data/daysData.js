a8d9fd2adfadd560f59bd0799f10bfee1a4786ee
Hind <hind.nayhi@gmail.com>
2022-09-05 16:03:55 -0600

    Create file to store the days array used to generate DayList stories.

---
+++ stories/data/daysData.js
@@ -0,0 +1,21 @@
+//Data used to generate stories for DayList in storis/index.js:
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+export default days;
