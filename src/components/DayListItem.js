6aec9e473ebbfe042fc2aba062210cf6f532e9fd
Hind <hind.nayhi@gmail.com>
2022-09-05 15:15:25 -0600

    Use classNames to implement custom classes depending on the props passed.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,18 @@
 import React from 'react'; //Optional since we have a newer version of React (above v17).
+import classNames from 'classnames';
+
+import 'components/DayListItem.scss';
 
 const DayListItem = (props) => {
+  const noSpots = props.spots === 0;
+
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': noSpots
+  });
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
