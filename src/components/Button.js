ac6fb3322d6ff24e0a9f17ff9e51d1e7b0368da0
Hind <hind.nayhi@gmail.com>
2022-09-05 12:56:50 -0600

    Implement 5 visual states for the Button component: base, confirm, danger, clickable and disabled.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,22 @@
 import React from "react";
-
 import "components/Button.scss";
 
 export default function Button(props) {
-  return <></>;
+  let buttonClass = 'button';
+
+  if (props.confirm) {
+    buttonClass += ' button--confirm';
+  }
+  if (props.danger) {
+    buttonClass += ' button--danger';
+  }
+
+  return (
+    <button 
+      onClick={props.onClick} 
+      disabled={props.disabled} 
+      className={buttonClass}>
+      {props.children}
+    </button>
+  );
 }
