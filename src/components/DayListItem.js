8a046fdba03dfd3be6aee6f8c5f55fbc5e0272b0
Hind <hind.nayhi@gmail.com>
2022-09-05 13:54:04 -0600

    Implement props + Add an onClick() event to set the day.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,9 +2,9 @@ import React from 'react'; //Optional since we have a newer version of React (ab
 
 const DayListItem = (props) => {
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
 }
