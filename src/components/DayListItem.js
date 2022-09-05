818f846e81c3ac5fe60703191f7e94320ec58235
Hind <hind.nayhi@gmail.com>
2022-09-05 15:38:40 -0600

    Implement formatSpots function to account for test cases where there's 0 or 1 spot.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,8 +4,18 @@ import classNames from 'classnames';
 import 'components/DayListItem.scss';
 
 const DayListItem = (props) => {
-  const noSpots = props.spots === 0;
 
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return `no spots remaining`;
+    }
+    if (spots === 1) {
+      return `1 spot remaining`;
+    }
+    return `${spots} spots remaining`;
+  };
+
+  const noSpots = props.spots === 0;
   const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': noSpots
@@ -14,7 +24,7 @@ const DayListItem = (props) => {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 };
