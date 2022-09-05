27f6c36bea61ff405462ef3f0443c7f0ff18fadb
Hind <hind.nayhi@gmail.com>
2022-09-05 16:02:28 -0600

    Create DayList component so it serves as a container for all the DayListItem components.

---
+++ src/components/DayList.js
@@ -0,0 +1,11 @@
+import React from 'react'; //Optional since we have a newer version of React (above v17).
+
+import DayListItem from './DayListItem';
+
+const DayList = () => {
+  return ( 
+    <ul></ul>
+  );
+}
+ 
+export default DayList;
