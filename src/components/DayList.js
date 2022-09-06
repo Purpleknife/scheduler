95b91d7b287d41218c9adbf74bf64bc68be76efc
Hind <hind.nayhi@gmail.com>
2022-09-06 10:14:47 -0600

    Change day to value and setDay to onChange in <DayList> to make it clear that the component is used to receive input from a user.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -12,8 +12,8 @@ const DayList = (props) => {
         id={day.id}
         name={day.name}
         spots={day.spots}
-        selected={day.name === props.day}
-        setDay={props.setDay}
+        selected={day.name === props.value}
+        setDay={props.onChange}
       />
     );
   });
--- stories/index.js
+++ stories/index.js
@@ -61,13 +61,13 @@ storiesOf("DayList", module)
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
   })
   .add("Monday", () => (
-    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Monday"} onChange={action("setDay")} />
   ))
   .add("Tuesday", () => (
-    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Tuesday"} onChange={action("setDay")} />
   ))
   .add("Wednesday", () => (
-      <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+      <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
   ));
 
 
