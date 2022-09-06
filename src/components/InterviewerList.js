b5b6950f2b2abed165e9f0e165cf46a7230ef111
Hind <hind.nayhi@gmail.com>
2022-09-06 10:19:17 -0600

    Change interviewer to value and setInterviewer to onChange in <InterviewerList> to make it clear that the component is used to receive user input.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -13,8 +13,8 @@ const InterviewerList = (props) => {
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === props.interviewer}
-        setInterviewer={() => props.setInterviewer(interviewer.id)}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
   });
--- stories/index.js
+++ stories/index.js
@@ -119,12 +119,12 @@ storiesOf("InterviewerList", module)
   .add("Selected", () => (
     <InterviewerList
       interviewers={interviewers}
-      interviewer={3}
+      value={3}
     />
   ))
   .add("Clickable", () => (
     <InterviewerList
       interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+      onChange={action("setInterviewer")}
     />
   ));
