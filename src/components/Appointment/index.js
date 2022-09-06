b94c063b79912a40096e0f51c7b3f4ffd651d4ac
Hind <hind.nayhi@gmail.com>
2022-09-06 10:49:33 -0600

    Create new component: Appointment to handle appointments made by the user.

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Appointment = (props) => {
+  return (
+    <article className="appointment"></article>
+  );
+};
+ 
+export default Appointment;
