5ba5f22166346a6e73a551276ef00bea067013b3
Hind <hind.nayhi@gmail.com>
2022-09-06 15:43:17 -0600

    Add an event.preventDefault() to <form> to prevent its default behavior: submit event.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -21,7 +21,7 @@ const Form = (props) => {
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form onSubmit={event => event.preventDefault()} autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
