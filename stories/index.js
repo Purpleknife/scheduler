5f970fc3ce09bdf5e14a65456f9f238d64abcb78
Hind <hind.nayhi@gmail.com>
2022-09-06 16:47:50 -0600

    Implement a story (Appointment Booked) to test the rendering of Show when props.interview is truthy.

--- stories/index.js
+++ stories/index.js
@@ -203,4 +203,14 @@ storiesOf("Appointment", module)
       <Appointment id={1} time="4pm" />
       <Appointment time="5pm" />
     </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
   ));
