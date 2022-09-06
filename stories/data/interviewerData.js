c3488a5da71af68b69ce5077cb4ea3a8d6e4edc3
Hind <hind.nayhi@gmail.com>
2022-09-06 08:55:30 -0600

    Add mock data to generate stories for new component: InterviewerListItem.

---
+++ stories/data/interviewerData.js
@@ -0,0 +1,9 @@
+//Data used to generate stories for InterviewerListItem in stories/index.js:
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+export default interviewer;
