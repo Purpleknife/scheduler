5bb6902e661d043c3dd16e5e2f3e16ce225c5982
Hind <hind.nayhi@gmail.com>
2022-09-06 09:26:49 -0600

    Create new component: InterviewerList to hold a list of interviewers.

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,31 @@
+import React from 'react'; //Optional since we have a newer version of React (above v17).
+
+import 'components/InterviewerList.scss';
+
+import InterviewerListItem from './InterviewerListItem';
+
+const InterviewerList = (props) => {
+
+  const interviewersData = props.interviewers;
+  const listInterviewers = interviewersData.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />
+    );
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{listInterviewers}</ul>
+    </section>
+  );
+}
+ 
+export default InterviewerList;
