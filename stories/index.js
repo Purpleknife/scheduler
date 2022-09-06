697683702bbb609ef3af2407441d9d1ad16b65fc
Hind <hind.nayhi@gmail.com>
2022-09-06 14:28:12 -0600

    Implement stories to test Error (child component of Appointment).

--- stories/index.js
+++ stories/index.js
@@ -20,6 +20,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 /* 
 ===> Initiates Storybook and registers our Button component.
@@ -173,4 +174,10 @@ storiesOf("Appointment", module)
   )
   .add("Status", () => 
     <Status message='Deleting' />
+  )
+  .add("Error", () => 
+    <Error 
+      message='Could not delete appointment.'
+      onClose={action('onClose')}
+    />
   );
