cfac14839dda509b66c0a5a51002939d8902d0c7
Hind <hind.nayhi@gmail.com>
2022-09-06 16:49:04 -0600

    Update <Show> to take in 2 props: student and interviewer, by implementing {...props.interview}.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -10,7 +10,7 @@ const Appointment = (props) => {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show /> : <Empty />}
+      {props.interview ? <Show {...props.interview}/> : <Empty />}
     </article>
   );
 };
