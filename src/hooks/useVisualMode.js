74e97e2d86727c3a41bb51fedc12b03e51d52eb1
Hind <hind.nayhi@gmail.com>
2022-09-08 09:58:50 -0600

    Implement the transition functionality: updating the current mode to a new mode.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,7 +4,17 @@ import { useState } from 'react';
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
 
-  return { mode };
+  const transition = (newMode) => {
+    if (newMode) {
+      return setMode(newMode); //If a new mode is passed, set it to the new mode.
+    }
+    setMode(mode); //Else, just keep the current one.
+  };
+
+  return { 
+    mode,
+    transition
+  };
 };
 
 export default useVisualMode;
