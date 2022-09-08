0d662d61622496d528e89c054ff1fb6b1f6b214b
Hind <hind.nayhi@gmail.com>
2022-09-08 09:07:54 -0600

    Update state, useEffect and <Appointment> to fetch data from api/interviewers.

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,31 +6,30 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from 'components/Appointment';
 
-import { getAppointmentsForDay } from 'helpers/selectors';
+import { getAppointmentsForDay, getInterview } from 'helpers/selectors';
 
 
 const Application = (props) => {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
-
+  console.log(state.interviewers);
   const setDay = day => setState({ ...state, day });
-
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   useEffect(() => {
     Promise.all([
       axios.get('api/days'),
       axios.get('api/appointments'),
-      //axios.get('api/interviewers'),
+      axios.get('api/interviewers'),
     ]).then((all) => {
       setState(prev => ({
         ...prev,
         days: all[0].data,
         appointments: all[1].data,
-        //interviewers: all[2].data
+        interviewers: all[2].data
       }));
     })
     .catch((error) => {
@@ -38,12 +37,16 @@ const Application = (props) => {
     });
   }, []); //The empty dependency array added prevents an infinite loop.
 
-
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const appointmentsList = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    
     return (
       <Appointment
         key={appointment.id}
-        {...appointment}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
       />
     );
   });
