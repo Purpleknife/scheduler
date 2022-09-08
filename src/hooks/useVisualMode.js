672bb01de95c8f7680130f5f68e3043bb1777354
Hind <hind.nayhi@gmail.com>
2022-09-08 12:10:50 -0600

    Implement the back functionality: whenever it's called, we should set the mode to the previous item in our history array.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,17 +3,41 @@ import { useState } from 'react';
 
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   const transition = (newMode) => {
-    if (newMode) {
-      return setMode(newMode); //If a new mode is passed, set it to the new mode.
+    setMode(newMode); //If a new mode is passed, set it to the new mode.
+    setHistory([newMode, ...history]);
+    //console.log('newMode', newMode);
+  };
+
+  
+  const back = () => {
+    const [lastAddedMode, ...rest] = history;
+    //console.log('prevMode', prevMode);
+    // console.log('zero', rest[0]);
+    // console.log('history', history);
+    //console.log('pop', history.pop());
+    // console.log('history length', history[history.length - 2]);
+    //console.log('slice', history.slice(0, -1));
+    //console.log('mode', mode);
+    if (history.length > 1) {
+      setMode(rest[0]);
+      setHistory([...rest]);
+      //console.log('rest', rest);
+      //console.log('rest...', [...rest]);
+      // console.log('history after slice', history);
+      // console.log('history after pop', history[history.length - 2]);
+      return;
     }
-    setMode(mode); //Else, just keep the current one.
+
+    setMode(history[0]);
   };
 
   return { 
     mode,
-    transition
+    transition,
+    back
   };
 };
 
