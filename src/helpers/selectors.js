07a2d5a63d69178994a644459bd4131de36b5ac1
Hind <hind.nayhi@gmail.com>
2022-09-08 09:09:19 -0600

    Add getInterview function and test it, to transform the data we get from the API into a usable structure.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,9 @@
 /*
 Selector: function that accepts state as an argument,
 and returns data that is derived from that state.
+
+=> These functions's goal is to transform the data we get
+from the API into a data structure that we can use in our components.
 */
 
 export const getAppointmentsForDay = (state, day) => {
@@ -14,5 +17,23 @@ export const getAppointmentsForDay = (state, day) => {
   const getDetailledAppointments = appointmentsForGivenDay.appointments.map(appointmentID => state.appointments[appointmentID]); //Based on the id, fetch the corresponding appointments from the array appointments that's inside appointmentsForGivenDay.
   
   return getDetailledAppointments;
+};
+
+
+
+export const getInterview = (state, interview) => {
+  let output = {};
+
+  if (!interview) { // If no data is passed, return null.
+    return null;
+  };
+
+  const interviewerID = interview.interviewer; //interview = appointment.interview ==> interview: { student: "Chad Takahashi", interviewer: 2 }.
+  const interviewer = state.interviewers[interviewerID];
+
+  output.student = interview.student;
+  output.interviewer = interviewer;
+
+  return output;  
 };
 
