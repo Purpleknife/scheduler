d0ad939395c4d2ddb33b55e55151481bb96e6bcd
Hind <hind.nayhi@gmail.com>
2022-09-12 15:35:10 -0600

    Implement updateSpots to update the remaining spots.

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -9,9 +9,7 @@ const useApplicationData = () => {
     interviewers: {}
   });
   
-
   const setDay = day => setState({ ...state, day });
-
 
   useEffect(() => {
     Promise.all([
@@ -32,9 +30,24 @@ const useApplicationData = () => {
   }, []); //The empty dependency array added prevents an infinite loop.
 
   
-  const bookInterview = (id, interview) => {
-    console.log(id, interview);
+  const updateSpots = (days, appointments) => {
+    const currentDay = days.find(day => day.name === state.day);
 
+    const appointIDs = currentDay.appointments;
+    const nullInterviews = appointIDs.filter((id) => !appointments[id].interview); //Get the number of appointments where interview is null.
+    const spotsAvailable = nullInterviews.length;
+      
+    const updatedDay = { ...currentDay, spots: spotsAvailable }; //Update the day without altering the original obj.
+
+    const currentDayIndex = days.findIndex(day => day.name === state.day); //Find the index so we can update the array days.
+    const updatedDays = [...days];
+    updatedDays[currentDayIndex] = updatedDay; //Update the days, done this way because it's an array, unlike 'day'.
+
+    setState(prev => ({...prev, days: updatedDays})); //Update the state.
+  };
+
+
+  const bookInterview = (id, interview) => {
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -46,7 +59,10 @@ const useApplicationData = () => {
     };
 
     return axios.put(`/api/appointments/${id}`, appointment)
-      .then(setState((prev) => ({...prev, appointments})))
+      .then(
+        setState((prev) => ({...prev, appointments})),
+        updateSpots(state.days, appointments)
+      )
       .catch((error) => {
         console.error(error);
       });
@@ -66,7 +82,10 @@ const useApplicationData = () => {
     };
 
     return axios.delete(`/api/appointments/${id}`, appointment)
-      .then(setState((prev) => ({...prev, appointments})))
+      .then(
+        setState((prev) => ({...prev, appointments})),
+        updateSpots(state.days, appointments)
+      )
       .catch((error) => {
         console.error(error);
       });
