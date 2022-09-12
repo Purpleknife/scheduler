524664d3cb6f56cb2d20b6299b83b9579ad253d9
Hind <hind.nayhi@gmail.com>
2022-09-12 09:01:22 -0600

    Update the Save button to make it functional.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -16,7 +16,11 @@ const Form = (props) => {
   const cancel = () => {
     reset();
     props.onCancel();
-  }
+  };
+
+  const save = () => {
+    props.onSave(student, interviewer);
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -40,7 +44,7 @@ const Form = (props) => {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel} >Cancel</Button>
-          <Button confirm onClick={props.onSave} >Save</Button>
+          <Button confirm onClick={save} >Save</Button>
         </section>
       </section>
     </main>
