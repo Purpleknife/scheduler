87ab219016d5404f5664294fe41b9afbcb5558f6
Hind <hind.nayhi@gmail.com>
2022-09-13 14:02:28 -0600

    Change to async and await.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,11 +6,16 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
-
-  return waitForElement(() => getByText("Monday")).then(() => {
+describe('Application', () => {
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText("Monday"));
+  
     fireEvent.click(getByText("Tuesday"));
+  
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
+
+
 });
