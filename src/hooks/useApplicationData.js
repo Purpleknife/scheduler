505c2bca68c58a4540826e8edee1f0cf326c2f9d
Hind <hind.nayhi@gmail.com>
2022-09-13 17:11:28 -0600

    Put WebSocket code in its own function.

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,66 +1,14 @@
 import { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from "reducers/application";
 
-const useApplicationData = () => {
 
-  const SET_DAY = "SET_DAY";
-  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-  const SET_INTERVIEW = "SET_INTERVIEW";
-  
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case SET_DAY:
-        return {
-          ...state,
-          day: action.day
-        }
-      case SET_APPLICATION_DATA:
-        return {
-          ...state,
-          days: action.days,
-          appointments: action.appointments,
-          interviewers: action.interviewers
-        }
-      case SET_INTERVIEW: //Implement the spots remaining functionality inside SET_INTERVIEW.
-        const updateSpots = (days, appointments) => {
-          const currentDay = days.find(day => day.name === state.day);
-      
-          const appointIDs = currentDay.appointments;
-          const nullInterviews = appointIDs.filter((id) => !appointments[id].interview); //Get the number of appointments where interview is null.
-          const spotsAvailable = nullInterviews.length;
-            
-          const updatedDay = { ...currentDay, spots: spotsAvailable }; //Update the day without altering the original obj.
-      
-          const currentDayIndex = days.findIndex(day => day.name === state.day); //Find the index so we can update the array days.
-          const updatedDays = [...days];
-          updatedDays[currentDayIndex] = updatedDay; //Update the days, done this way because it's an array, unlike 'day'.
-      
-          return updatedDays;
-        };
-
-        const appointment = {
-          ...state.appointments[action.id],
-          interview: action.interview ? { ...action.interview } : null
-        };
-    
-        const appointments = {
-          ...state.appointments,
-          [action.id]: appointment
-        };
-
-        return {
-          ...state,
-          days: updateSpots(state.days, appointments),
-          appointments
-        }
-      
-      default:
-        throw new Error(
-          `Tried to reduce with unsupported action type: ${action.type}`
-        );
-    }
-  };
+const useApplicationData = () => { 
 
   const initialState = {
     day: "Monday",
@@ -91,17 +39,7 @@ const useApplicationData = () => {
     });
 
     //WebSocket implementation:
-    const URL = process.env.REACT_APP_WEBSOCKET_URL;
-    const ws = new WebSocket(URL);
-
-      ws.addEventListener('message', (event) => {
-        const { type, id, interview } = JSON.parse(event.data);
-
-        if (type === 'SET_INTERVIEW') {
-          dispatch({ type, id, interview });
-        }
-      });
-    return () => ws.close(); //To close the ws connection.
+    webSocketUpdate();
     
 
   }, []); //The empty dependency array added prevents an infinite loop.
@@ -127,6 +65,21 @@ const useApplicationData = () => {
       .catch((error) => {
         console.error('Could not complete DELETE request:', error);
       });
+  };
+
+
+  const webSocketUpdate = () => {
+    const URL = process.env.REACT_APP_WEBSOCKET_URL;
+    const ws = new WebSocket(URL);
+  
+      ws.addEventListener('message', (event) => {
+        const { type, id, interview } = JSON.parse(event.data);
+  
+        if (type === 'SET_INTERVIEW') {
+          dispatch({ type, id, interview });
+        }
+      });
+    return () => ws.close(); //To close the ws connection.
   };
 
 
